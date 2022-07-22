5a27584e63c8da6352bf68a6300976a1eda2ee54
Сергей Воробьев <kazan1003@gmail.com>
2022-07-22 15:50:19 +0300

    Добавьте файлы проекта.

---
+++ Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MediatR;
+
+namespace Notes.Application.Notes.DeleteCommand
+{
+    /// <summary>
+    /// команда удаления заметки
+    /// </summary>
+    public class DeleteNoteCommand : IRequest
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+    }
+}
