73117f93f93168b20a048be6c68ae7d1ea2aeb28
Сергей Воробьев <kazan1003@gmail.com>
2022-07-25 17:01:39 +0300

    in work

---
+++ Notes.Application/DepedencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Notes.Application
+{
+    /// <summary>
+    /// регистрирует Медиатр с помощью специального метода addMediatr
+    /// </summary>
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
